edce444eab658e28a1bb6dd17d4a191e9f1f436a
Vladman <thewakosmanclub@gmail.com>
2016-03-11 12:39:34 -0600

    pruea

---
+++ prueba.js
@@ -0,0 +1,42 @@
+var omx = require('omx-manager');
+var express = require('express')
+  , app = express()  
+  , server = require('http').createServer(app)
+  , path = require('path')
+  , io = require('socket.io').listen(server);
+
+
+
+// all environments
+app.set('port', process.env.TEST_PORT || 8080);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(omx());
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+
+//Routes
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
+});
+
+app.get('/remote', function (req, res) {
+  res.sendfile(__dirname + '/public/remote.html');
+});
+
+//Socket.io Config
+io.set('log level', 1);
+
+server.listen(app.get('port'), function(){
+  console.log('Prubea is running on port ' + app.get('port'));
+});
+
+omx.play(['1.MP4','2.MP4','3.mp4','4.MP4','5.MP4']);
+
+
